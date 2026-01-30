805132c1a9a077ef87442ef220531ffeea0caa91
Schon, Maximilian <mschon@kzvnr.de>
2026-01-30 17:12:09 +0100

    WIP: cli

--- src/alpha_cli/cli.zig
+++
@@ -1,9 +0,0 @@
-pub const help_text =
-    \\ Usage: as <command> [...args]
-    \\      create  My-Project  (TBD) Create a new project from a template
-    \\      run     ./main.as   (TBD) Execute a file
-    \\      tinker              (TBD) Start a REPL session
-    \\      lint                (TBD) Lint source files
-    \\      test                (TBD) Run unit tests
-    \\      <command> --help    Print help text for a command
-;
